24dcb6e9e83cce3774c872af2f795dca562b964f
fug4 <38590418+fug4@users.noreply.github.com>
2021-08-28 15:40:25 +0800

    preloads

---
+++ preloads.js
@@ -0,0 +1,11 @@
+function preload() {
+  worldMap = loadImage('./images/map0825.png')
+  hlALL = loadImage('./images/highlights/highlight_all_50.png')
+  hlJP = loadImage('./images/highlights/highlight_Japan.png')
+  hlCN = loadImage('./mages/highlights/highlight_China.png')
+  hlMZ = loadImage('./images/highlights/highlight_MingZheng.png')
+  hlNL = loadImage('./mages/highlights/highlight_Netherlands.png')
+  hlSPN = loadImage('./images/highlights/highlight_Spain.png') 
+  imgL = loadImage('./images/manchu0825_L.png'); // 圖片788 × 2621
+  imgR = loadImage('./images/manchu0825_R.png'); // 圖片723 × 2425
+}
